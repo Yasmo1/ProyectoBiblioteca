373a0dce1bb2c19e8fba4efae798e47e4345efed
yasmo <yasmoyplh@gmail.com>
2018-05-28 10:30:14 +0200

    Integrado EasyAdminBundle y FOSUserBundle, adema de creado los Primeros Usuarios

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+
+class AdminController extends BaseAdminController
+{
+    public function createNewUserEntity()
+
+    {
+
+        return $this->get('fos_user.user_manager')->createUser();
+
+    }
+
+    public function prePersistUserEntity($user)
+
+    {
+
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+
+    }
+
+    public function preUpdateUserEntity($user)
+
+    {
+
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+
+    }
+}
