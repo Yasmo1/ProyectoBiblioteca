b4b9999626acbb1eebac5811ea70154b61ff8b6f
yasmo <yasmoyplh@gmail.com>
2018-05-31 17:01:34 +0200

    Montando la Entidad Noticia En SonataAdminBundle

---
+++ src/Admin/TematicaNoticiaAdmin.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class TematicaNoticiaAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Meta data', ['class' => 'col-md-4'])
+                ->add('nombre', TextType::class)
+                ->add('descripcion', TextareaType::class)
+            ->end()
+        ;
+
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('nombre');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('nombre');
+    }
+}
