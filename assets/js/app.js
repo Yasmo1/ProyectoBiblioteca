51c60c8bfea1f8fcd3f06fbb02c9b8a58e0e73f5
yasmo <yasmoyplh@gmail.com>
2018-06-14 14:07:18 +0200

    Subiendo los ficheros css y js ademas de Bootstrap en su ultima version 4.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,12 @@
-
-// assets/js/app.js
-
-// loads the jquery package from node_modules
 var $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap');
 
-// import the function from greet.js (the .js extension is optional)
-// ./ (or ../) means to look for a local file
-var greet = require('./greet');
+// or you can include specific pieces
+// require('bootstrap-sass/javascripts/bootstrap/tooltip');
+// require('bootstrap-sass/javascripts/bootstrap/popover');
 
 $(document).ready(function() {
-    $('body').prepend('<h1>'+greet('john')+'</h1>');
+    $('[data-toggle="popover"]').popover();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,29 +7,30 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
 
-    // will create public/build/app.js and public/build/app.css
-    .addEntry('app', './assets/js/app.js')
-
-    // allow legacy applications to use $/jQuery as a global variable
-    .autoProvidejQuery()
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
 
     // enable source maps during development
     .enableSourceMaps(!Encore.isProduction())
 
-    // empty the outputPath dir before each build
-    .cleanupOutputBeforeBuild()
+    // will create public/build/app.js and public/build/app.css
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/css/global.scss')
 
     // show OS notifications when builds finish/fail
-    .enableBuildNotifications()
+    //.enableBuildNotifications()
 
     // create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning()
 
     // allow sass/scss files to be processed
-    // .enableSassLoader()
-    .enableSassLoader(function(sassOptions) {}, {
-        resolveUrlLoader: false
-    })
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+    //.enableSassLoader(function(sassOptions) {}, {
+        //resolveUrlLoader: false
+    //})
 ;
 
 // export the final configuration
